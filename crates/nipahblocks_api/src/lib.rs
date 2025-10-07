c1083cbb842743f7fe106931ed1eb9ee5776d185
Denis Strizhkin <strdenis02@gmail.com>
2025-10-07 12:24:59 +0300

    chunk and position

--- crates/nipahblocks_api/src/lib.rs
+++ crates/nipahblocks_api/src/lib.rs
@@ -21,38 +21,95 @@ pub enum DeserializeError {
     NotBinary,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum PlayerMessage {
-    Message(String),
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+pub struct Position {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct ChunkId {
+    x: i16,
+    y: i16,
+}
+
+impl From<Position> for ChunkId {
+    fn from(value: Position) -> Self {
+        ChunkId {
+            x: value.x.round() as i16,
+            y: value.y.round() as i16,
+        }
+    }
+}
+
+impl From<ChunkId> for Position {
+    fn from(value: ChunkId) -> Self {
+        Position {
+            x: value.x as f32,
+            y: value.y as f32,
+            z: 0.0,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum PlayerMessage {
+    Message(String),
+    UpdatePosition(Position),
+    FetchChunk(ChunkId),
+    FetchPlayers,
+}
+
+pub type PlayerId = u16;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ChatMessage {
-    pub user_id: u16,
+    pub user_id: PlayerId,
     pub content: String,
     pub time: DateTime<Utc>,
+}
+
+pub const CHUNK_WIDTH: usize = 16;
+pub const CHUNK_HEIGHT: usize = 256;
+pub const CHUNK_SIZE: usize = CHUNK_WIDTH * CHUNK_WIDTH * CHUNK_HEIGHT;
+
+pub type BlockId = u8;
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+pub struct Block(BlockId);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Chunk {
+    id: ChunkId,
+    blocks: Vec<Block>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum ServerMessage {
     ChatMessage(ChatMessage),
-    PlayerConnected(u16),
-    PlayerDisconnected(u16),
+    Players(Vec<PlayerId>),
+    PlayerConnected(PlayerId),
+    PlayerDisconnected(PlayerId),
+    PlayerMoved(PlayerId, Position),
+    Chunk(Chunk),
 }
 
 impl TryFrom<ServerMessage> for tungstenite::Message {
     type Error = SerializeError;
     fn try_from(value: ServerMessage) -> Result<Self, Self::Error> {
-        let data = bincode::serde::encode_to_vec(value, config::standard())?;
-        Ok(tungstenite::Message::Binary(data.into()))
+        bincode::serde::encode_to_vec(value, config::standard())
+            .map(|data| tungstenite::Message::Binary(data.into()))
+            .map_err(SerializeError::EncodeError)
     }
 }
 
 impl TryFrom<PlayerMessage> for tungstenite::Message {
     type Error = SerializeError;
     fn try_from(value: PlayerMessage) -> Result<Self, Self::Error> {
-        let data = bincode::serde::encode_to_vec(value, config::standard())?;
-        Ok(tungstenite::Message::Binary(data.into()))
+        bincode::serde::encode_to_vec(value, config::standard())
+            .map(|data| tungstenite::Message::Binary(data.into()))
+            .map_err(SerializeError::EncodeError)
     }
 }
 
